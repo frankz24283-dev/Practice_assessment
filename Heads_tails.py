8d5800c98159872fa2a343d555be53ac5e3f0f63
frankz24283-dev <frankz.24283@obhs.school.nz>
2026-08-15 14:59:45 +1200

    Fourth

--- Heads_tails.py
+++ Heads_tails.py
@@ -13,7 +13,14 @@ import random
 print("Welcome to Heads & Tails game! ")
 name = str(input("Enter you name: "))
 age = int(input("Enter you age: "))
+options = ["Heads", "Tails"]
 
+# Keep score inside the function
 def heads_tails():
     user_score = 0
     computer_score = 0
+
+#Keep playing until computer or user reaches 2 points
+while user_score != 2 and computer_score != 2:
+    random_choice = random.randint(0,1)
+    computer_guess = options[random.choice]
