1e257e79c4dffac42774d3a039a796122baf7af4
frankz24283-dev <frankz.24283@obhs.school.nz>
2026-08-15 22:26:39 +1200

    Update Heads_Tails(Merit).py

--- Heads_Tails(Merit).py
+++ Heads_Tails(Merit).py
@@ -18,16 +18,18 @@ def heads_tails(player_name):
     while player_score != 2 and opponent_score != 2:
         random_index = random.randint(0,1)
         computer_choice = coin_options[random_index]
-        player_input = input("Heads or Tails? ")
-        formatted_player_guess = player_input. capitalize()
-    if formatted_player_guess == computer_choice:
-        player_score += 1
-        print(f"It was {computer_choice}, you guessed {formatted_player_guess}. You won that round.")
-    else:
-        opponent_score += 1
-        print(f"It was {computer_choice}. You guessed {formatted_player_guess}. You lost that round.")
 
-    print(f"Player Score: {player_score} | Computer Score: {opponent_score}\n")
+        player_input = input("Heads or Tails? ")
+        formatted_player_guess = player_input.capitalize()
+
+        if formatted_player_guess == computer_choice:
+            player_score += 1
+            print(f"It was {computer_choice}, you guessed {formatted_player_guess}. You won that round.")
+        else:
+            opponent_score += 1
+            print(f"It was {computer_choice}. You guessed {formatted_player_guess}. You lost that round.")
+
+        print(f"Player Score: {player_score} | Computer Score: {opponent_score}\n")
 
     if player_score == 2:
         print(f"Congratulations {player_name}, you won the game!")
@@ -37,5 +39,4 @@ def heads_tails(player_name):
 print("Welcome to Heads & Tails game!")
 player_name = input("Enter your name: ")
 player_age = int(input("Enter your age: "))
-
 heads_tails(player_name)
